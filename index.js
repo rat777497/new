e22efdda59a8044699b1b69bb3737146fe6d1660
rat777497 <rat777497@gmail.com>
2025-03-28 15:23:22 +0300

    commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var home = document.getElementById("home")
+var guest = document.getElementById("geust")
+    var count = 0;
+    var count2 = 0;
+
+
+function home1(){
+    count +=1
+    home.textContent = count  ; 
+    
+}
+function home2(){
+    count +=2
+    home.textContent = count  ; 
+    
+}
+function home3(){
+    count +=3
+    home.textContent = count  ; 
+    
+}
+function guest1(){
+    count2 +=1
+    guest.textContent = count2  ; 
+    
+}
+function guest2(){
+    count2 +=2
+    guest.textContent = count2  ; 
+    
+}
+function guest3(){
+    count2 +=3
+    guest.textContent = count2  ; 
+    
+}
